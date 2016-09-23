4cae0ec5d1721556566b72a1661da1266ccfc5b9
ACER-E1_471G\ACER <srachmandani@gmail.com>
2016-09-23 20:49:10 +0700

    Tulung insert rating euy

--- application/models/rating_model.php
+++ application/models/rating_model.php
@@ -89,7 +89,53 @@ class rating_model extends CI_Model {
         $order_by = array("value" => 'DESC');
         return $this->get($select, $array_where, false, false, false, $order_by, $group_by);
     }
+	function get_by_user_id_and_estates_id($user_id,$estates_id) {
+		$select = '*';
+		$array_where = array('user_id' => $user_id,'estates_id'=>$estates_id);
+		$array_like = array();
+		$order_by = array();
+		return $this -> get(($select, $array_where, false, false, false, $order_by, $group_by);;
+	}
+	function get_total_rating(){
+		$this -> db -> select('(SUM(value)/COUNT(*)) as total_rating');
+		$this -> db -> from('rating');
+		$query = $this -> db -> get();
+		$rows = $query -> result();
+		$query -> free_result();
+		return $rows[0] -> total_rating;
+	}
+	function get_total_rating_by_estates_id($id){
+		$this -> db -> select('(SUM(value)/COUNT(*)) as total_rating');
+		$this -> db -> where(array('estates_id'=>$id));
+		$this -> db -> from('rating');
+		$query = $this -> db -> get();
+		$rows = $query -> result();
+		$query -> free_result();
+		return $rows[0] -> total_rating;
+	}
+	function insert($data_array) {
+		$data_array['created_at']=date('Y-m-d H:i:s');
+		$data_array['updated_at']=date('Y-m-d H:i:s');
+		$this -> db -> insert('rating', $data_array);
+		return $this -> db -> insert_id();
+	}
+	public function remove($arr_where) {
+		$this -> db -> where($arr_where);
+		$this -> db -> delete('rating');
+		return $this->db->affected_rows();
+	}
 
+	public function remove_by_id($id) {
+		$array_where = array('id' => $id);
+		return $this -> remove($array_where);
+	}
+
+	function update($data_array, $array_where) {
+		$this -> db -> where($array_where);
+		$data_array['created_at']=date('Y-m-d H:i:s');
+		$data_array['updated_at']=date('Y-m-d H:i:s');
+		$this -> db -> update('rating', $data_array);
+	}
     function get_by_estates_id($id) {
         $select = '*';
         $array_where = array('estates_id' => $id);
